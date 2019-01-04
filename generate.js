2c5c34321839f58c66bfafb7ab9904df131ac69f
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2019-01-04 11:52:11 -0600

    Update generate.js

--- generate.js
+++ generate.js
@@ -1,7 +1,18 @@
 AI.prototype.generate=function(){
   var out="";
   for(var i=0;i<2;i++){
-    out+=this.orders[Math.trunc(Math.random()*this.orders.length)]
+    out+=translate(this.orders[Math.trunc(Math.random()*this.orders.length)]);
     out+=" ";
   }
+  document.getElementById("output").innerHTML=out;
+}
+
+function translate(c){
+  var chars="abcdefghijklmnopqrstuvwxyz";
+  var d="";
+  chars+=chars.toUpperCase();
+  for(var ii=0;ii<c.length;ii++){
+    d+=chars.charAt(c[ii]);
+  }
+  return c;
 }
