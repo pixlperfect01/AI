cbea551aed01ce6af3a9fde1faa213f1d77d4e0d
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2019-01-04 11:59:08 -0600

    Update AI.js

--- AI.js
+++ AI.js
@@ -12,6 +12,24 @@ function AI(){
       }
     }
   }
+  this..generate=function(){
+    var out="";
+    for(var i=0;i<2;i++){
+      out+=translate(this.orders[Math.trunc(Math.random()*this.orders.length)]);
+      out+=" ";
+    }
+    document.getElementById("output").innerHTML=out;
+  }
+}
+
+function translate(c){
+  var chars="abcdefghijklmnopqrstuvwxyz";
+  var d="";
+  chars+=chars.toUpperCase();
+  for(var ii=0;ii<c.length;ii++){
+    d+=chars.charAt(c[ii]);
+  }
+  return c;
 }
 
 function numOf(a, b){
