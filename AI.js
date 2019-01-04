670afc985bfac26410253f9fefc0e66c2e76670b
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2019-01-04 11:36:54 -0600

    Update AI.js

--- AI.js
+++ AI.js
@@ -1,6 +1,25 @@
 //this file is just a housing file the code is in the other two
 function AI(){
   this.info=[];
+  this.orders=[];
   this.commonOrders=[];
   this.vals=["abcdefghijklmnopqrstuvwxyz"];
+  this.sort=function(){
+    for(var i=0;i<this.orders.length;i++){
+      numOf(this.orders[i],this.orders);
+    }
+  }
+}
+
+function numOf(a, b){
+  if(!a||!b){
+    return;
+  }
+  var out=0;
+  for(var ii=0;ii<b.length;ii++){
+    if(b[ii]===a){
+      out++;
+    }
+  }
+  return out;
 }
